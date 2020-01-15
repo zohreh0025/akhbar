03797457608776a8f98e5a3b229c74ca88d1de89
zohrehAndroid <zohreh1455@gmail.com>
2020-01-15 01:32:06 -0800

    fourcommit

--- app/src/main/java/ir/akhbar/newsviewholder.java
+++ app/src/main/java/ir/akhbar/newsviewholder.java
@@ -7,12 +7,17 @@ import android.widget.TextView;
 
 public class newsviewholder extends RecyclerView.ViewHolder{
     private TextView titell;
+    private TextView dec;
+
     public newsviewholder(View itemView) {
         super(itemView);
         titell= (TextView) itemView.findViewById(R.id.titell1);
+        dec= (TextView) itemView.findViewById(R.id.description);
     }
     public void bind(NewsData data){
+
         titell.setText(data.getTitle());
+        dec.setText(data.getDescription());
     }
 
 }
